a897009a89b917d8b68609e2e72c33e7a1b95967
Victor <super_vg@bk.ru>
2021-06-03 18:57:02 +0300

    create component in Dialogs.jsx, Profile.jsx and rewrite style

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,27 +1,38 @@
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
+const DialogItem = (props) => {
+  let path = '/dialogs/' + props.id
+
+  return <div className={s.dialog + ' ' + s.active}>
+    <NavLink to={path}>{props.name}</NavLink>
+  </div>
+}
+
+const Message = (props) => {
+  return <div className={s.message}>{props.message}</div>
+}
+
 const Dialogs = (props) => {
   return (
     <div className={s.dialogs}>
+
       <div className={ s.dialogsItems}>
-        <div className={s.dialog + ' ' + s.active}>
-          <NavLink to='/dialogs/1'>Victor</NavLink>
-        </div>
-        <div className={s.dialog}>
-          <NavLink to='/dialogs/2'>Dim</NavLink>
-        </div>
-        <div className={s.dialog}>
-          <NavLink to='/dialogs/3'>Andrey</NavLink>
-        </div>
-        <div className={s.dialog}>
-          <NavLink to='/dialogs/4'>Artur</NavLink>
-        </div>
+
+        <DialogItem name='Victor' id='1'/>
+        <DialogItem name='Dim' id='2'/>
+        <DialogItem name='Andrey' id='3'/>
+        <DialogItem name='Artur' id='4'/>
+        <DialogItem name='Ivan' id='5'/>
+        <DialogItem name='Vasia' id='6'/>
+        <DialogItem name='Lena' id='7'/>
+
+
       </div>
       <div className={s.messages}>
-        <div className={s.message}>Hello</div>
-        <div className={s.message}>How are you?</div>
-        <div className={s.message}>Ok!</div>
+        <Message message='Hello'/>
+        <Message message='How are you?'/>
+        <Message message='Ok!'/>
       </div>
     </div>
   )
