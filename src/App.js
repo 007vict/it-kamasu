2a4efe280a6eefc0d696b0ad0cc49c9e962659ed
Victor <super_vg@bk.ru>
2021-06-10 12:59:35 +0300

    transfer data to state.js

--- src/App.js
+++ src/App.js
@@ -18,11 +18,17 @@ const App = (props) => {
         <Header/>
         <Navbar/>
         <div className='app-wrapper-content'>
-          <Route path='/profile' render={ () => <Profile posts={props.posts}/> } />
-          <Route path='/dialogs' render={ () => <Dialogs message={props.message} dialogs={props.dialogs}/> } />
-          <Route path='/news' render={ () => <News/> } />
-          <Route path='/music' render={ () => <Music/> } />
-          <Route path='/settings' render={ () => <Settings/> } />
+          <Route path='/profile'
+                 render={() => <Profile
+                   state={props.state.profilePage}/>}
+          />
+          <Route path='/dialogs'
+                 render={() => <Dialogs
+                   state={props.state.dialogsPage}/>}
+          />
+          <Route path='/news' render={() => <News/>}/>
+          <Route path='/music' render={() => <Music/>}/>
+          <Route path='/settings' render={() => <Settings/>}/>
         </div>
       </div>
     </BrowserRouter>
---
+++ src/redux/state.js
@@ -0,0 +1,28 @@
+
+let state = {
+  profilePage: {
+    posts: [
+      {id: 1, message: 'Hi!How are you?', likesCount: 55},
+      {id: 2, message: 'It\'s my first post))', likesCount:15},
+      {id: 3, message: 'Ok!'}
+    ],
+  },
+  dialogsPage: {
+    messages: [
+      {id: 1, message: 'Hello'},
+      {id: 2, message: 'How are you?'},
+      {id: 3, message: 'Ok!'}
+    ],
+    dialogs: [
+      {id: 1, name: 'Victor'},
+      {id: 2, name: 'Dim'},
+      {id: 3, name: 'Andrey'},
+      {id: 4, name: 'Artur'},
+      {id: 5, name: 'Ivan'},
+      {id: 6, name: 'Vania'},
+      {id: 7, name: 'Lena'}
+    ]
+  },
+}
+
+export default state
