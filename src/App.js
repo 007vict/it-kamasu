8ee2a3dd3ebdc3c166893dd1aa33487622ffbae4
Victor <super_vg@bk.ru>
2021-06-19 21:40:36 +0300

    callback over props

--- src/App.js
+++ src/App.js
@@ -9,28 +9,29 @@ import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import {BrowserRouter, Route} from 'react-router-dom';
 
-
 const App = (props) => {
 
   return (
 
-      <div className='app-wrapper'>
-        <Header/>
-        <Navbar/>
-        <div className='app-wrapper-content'>
-          <Route path='/profile'
-                 render={() => <Profile
-                   state={props.state.profilePage}/>}
-          />
-          <Route path='/dialogs'
-                 render={() => <Dialogs
-                   state={props.state.dialogsPage}/>}
-          />
-          <Route path='/news' render={() => <News/>}/>
-          <Route path='/music' render={() => <Music/>}/>
-          <Route path='/settings' render={() => <Settings/>}/>
-        </div>
+    <div className='app-wrapper'>
+      <Header/>
+      <Navbar/>
+      <div className='app-wrapper-content'>
+        <Route path='/profile'
+               render={() => <Profile
+                 state={props.state.profilePage}
+                 addPost={props.addPost}
+               />}
+        />
+        <Route path='/dialogs'
+               render={() => <Dialogs
+                 state={props.state.dialogsPage}/>}
+        />
+        <Route path='/news' render={() => <News/>}/>
+        <Route path='/music' render={() => <Music/>}/>
+        <Route path='/settings' render={() => <Settings/>}/>
       </div>
+    </div>
   );
 }
 
