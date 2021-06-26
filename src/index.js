9fc2f025c46a8a16ea80cb42c679a7a22b6e18a9
Victor <super_vg@bk.ru>
2021-06-26 21:05:08 +0300

    delete render and create observer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
-import {rerenderEntireTree} from "./render";
-import state from "./redux/state";
+import state, {subscribe} from "./redux/state";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import {addPost, updateNewPostText} from "./redux/state";
+import {BrowserRouter} from "react-router-dom";
+
+
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App state={state}
+             addPost={addPost}
+             updateNewPostText={updateNewPostText}
+        />
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+subscribe(rerenderEntireTree)
+
+reportWebVitals();
+
 
 rerenderEntireTree(state)
 
