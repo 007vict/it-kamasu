684888755f5ebf237170607628d1f57f8873bbc3
Victor <super_vg@bk.ru>
2021-06-29 12:39:39 +0300

    dispatch action

--- src/redux/state.js
+++ src/redux/state.js
@@ -3,7 +3,7 @@ let store = {
     profilePage: {
       posts: [
         {id: 1, message: 'Hi!How are you?', likesCount: 55},
-        {id: 2, message: 'It\'s my first post))', likesCount:15},
+        {id: 2, message: 'It\'s my first post))', likesCount: 15},
         {id: 3, message: 'Ok!'}
       ],
       newPostText: 'it-camara'
@@ -25,28 +25,31 @@ let store = {
       ]
     },
   },
-  getState() {
-    return this._state
-  },
   _callSubscriber() {
     console.log('State changed!')
   },
-  addPost() {
-    let newPost = {
-      id: 5,
-      message: this._state.profilePage.newPostText,
-      likesCount: 0
-    }
-    this._state.profilePage.posts.push(newPost)
-    this._state.profilePage.newPostText = ''
-    this._callSubscriber(this._state)
-  },
-  updateNewPostText(newText) {
-    this._state.profilePage.newPostText = newText
-    this._callSubscriber(this._state)
+
+  getState() {
+    return this._state
   },
   subscribe(observer) {
     this._callSubscriber = observer
+  },
+
+  dispatch(action) {
+    if (action.type === 'ADD-POST') {
+      let newPost = {
+        id: 5,
+        message: this._state.profilePage.newPostText,
+        likesCount: 0
+      }
+      this._state.profilePage.posts.push(newPost)
+      this._state.profilePage.newPostText = ''
+      this._callSubscriber(this._state)
+    } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+      this._state.profilePage.newPostText = action.newText
+      this._callSubscriber(this._state)
+    }
   }
 }
 
