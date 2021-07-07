5c03310a62d0b34ebaf894ce1d13e40bf66d00a2
007vict <super_vg@bk.ru>
2021-07-07 16:11:01 +0300

    add new message post in dialogs

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,5 +1,8 @@
 const ADD_POST = 'ADD-POST';
-const updateNewPostText = 'UPDATE-NEW-POST-TEXT';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
+const SEND_MESSAGE = 'SEND_MESSAGE';
 
 
 let store = {
@@ -26,7 +29,8 @@ let store = {
         {id: 5, name: 'Ivan'},
         {id: 6, name: 'Vania'},
         {id: 7, name: 'Lena'}
-      ]
+      ],
+      newMessageBody: ''
     },
   },
   _callSubscriber() {
@@ -50,18 +54,28 @@ let store = {
       this._state.profilePage.posts.push(newPost)
       this._state.profilePage.newPostText = ''
       this._callSubscriber(this._state)
-    } else {
-      if (action.type === updateNewPostText) {
-        this._state.profilePage.newPostText = action.newText
-        this._callSubscriber(this._state)
-      }
+    } else if (action.type === UPDATE_NEW_POST_TEXT) {
+      this._state.profilePage.newPostText = action.newText
+      this._callSubscriber(this._state)
+    } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
+      this._state.dialogsPage.newMessageBody = action.body
+      this._callSubscriber(this._state)
+    }else if (action.type === SEND_MESSAGE) {
+      let body = this._state.dialogsPage.newMessageBody
+      this._state.dialogsPage.newMessageBody = ''
+      this._state.dialogsPage.messages.push({id: 4, message: body})
+      this._callSubscriber(this._state)
     }
   }
 }
 
 export const addPostActionCreator = () => ({type: ADD_POST})
 export const updateNewPostTextActionCreator = (text) =>
-  ({type: 'UPDATE-NEW-POST-TEXT', newText: text })
+  ({type: UPDATE_NEW_POST_TEXT, newText: text})
+
+export const sendMessageCreator = () => ({type: SEND_MESSAGE})
+export const updateNewMessageBodyCreator = (body) =>
+  ({type: UPDATE_NEW_MESSAGE_BODY, body: body})
 
 export default store
 window.store = store
