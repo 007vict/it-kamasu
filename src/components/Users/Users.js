683aa263d68d3d453e85d527961b5b134e87bc25
007vict <super_vg@bk.ru>
2021-07-13 13:24:28 +0300

    profile page, ajax, api

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,6 +1,7 @@
 import styles from "./Users.module.css";
 import userPhoto from "../../assets/images/user1.png";
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 
 let Users = (props) => {
@@ -27,10 +28,12 @@ let Users = (props) => {
       props.users.map(u => <div key={u.id}>
         <span>
           <div>
-            <img src={u.photos.small != null ? u.photos.small : userPhoto}
-                 className={styles.userPhoto}
-                 alt=''
-            />
+            <NavLink to={'/profile/' + u.id}>
+              <img src={u.photos.small != null ? u.photos.small : userPhoto}
+                   className={styles.userPhoto}
+                   alt=''
+              />
+            </NavLink>
           </div>
           <div>
             {u.followed
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,5 +1,6 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const SET_USER_PROFILE = 'SET_USER_PROFILE';
 
 let initialState = {
   posts: [
@@ -7,7 +8,8 @@ let initialState = {
     {id: 2, message: 'It\'s my first post))', likesCount: 15},
     {id: 3, message: 'Ok!'}
   ],
-  newPostText: 'it-camara'
+  newPostText: 'it-camara',
+  profile: null
 }
 
 const profileReducer = (state = initialState, action) => {
@@ -30,12 +32,16 @@ const profileReducer = (state = initialState, action) => {
         newPostText: action.newText
       }
     }
+    case SET_USER_PROFILE: {
+      return {...state, profile: action.profile}
+    }
     default:
       return state
   }
 }
 
 export const addPostActionCreator = () => ({type: ADD_POST})
+export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile})
 export const updateNewPostTextActionCreator = (text) =>
   ({type: UPDATE_NEW_POST_TEXT, newText: text})
 
