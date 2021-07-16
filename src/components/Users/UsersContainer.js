3a2ad63e2665bb524a9fefcfb02d96b07069a6d8
007vict <super_vg@bk.ru>
2021-07-16 18:16:32 +0300

    hoc high order component

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -3,6 +3,7 @@ import {connect} from "react-redux";
 import Users from "./Users";
 import Preloader from "../common/Preloader/Preloader";
 import {follow, getUsers, setCurrentPage, toggleFollowingProgress, unfollow} from "../../redux/users-reducer";
+import {withAuthRedirect} from "../hoc/withAuthRedirect";
 
 class UsersContainer extends React.Component {
 
@@ -43,7 +44,5 @@ let mapStateToProps = (state) => {
 }
 
 
-export default connect(mapStateToProps, {
-  follow,unfollow, setCurrentPage, toggleFollowingProgress, getUsers
-})
-(UsersContainer)
+export default withAuthRedirect(connect(mapStateToProps, {
+  follow,unfollow, setCurrentPage, toggleFollowingProgress, getUsers})(UsersContainer))
