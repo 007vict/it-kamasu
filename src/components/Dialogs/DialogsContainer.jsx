fdbd5bf901af6e2f37bb75e066353016a704ce25
007vict <super_vg@bk.ru>
2021-07-17 09:46:31 +0300

    compose

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,8 +1,8 @@
 import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/dialogs-reducer";
 import Dialogs from "./Dialogs";
 import {connect} from "react-redux";
-import React from "react";
 import {withAuthRedirect} from "../hoc/withAuthRedirect";
+import {compose} from "redux";
 
 let mapStateToProps = (state) => {
   return {
@@ -21,9 +21,5 @@ let mapDispatchToProps = (dispatch) => {
   }
 }
 
-let AuthRedirectComponent = withAuthRedirect(Dialogs)
-
-
-const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(AuthRedirectComponent)
-
-export default DialogsContainer
+export default compose(connect(mapStateToProps, mapDispatchToProps),
+  withAuthRedirect)(Dialogs)
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -4,6 +4,7 @@ import Users from "./Users";
 import Preloader from "../common/Preloader/Preloader";
 import {follow, getUsers, setCurrentPage, toggleFollowingProgress, unfollow} from "../../redux/users-reducer";
 import {withAuthRedirect} from "../hoc/withAuthRedirect";
+import {compose} from "redux";
 
 class UsersContainer extends React.Component {
 
@@ -43,6 +44,5 @@ let mapStateToProps = (state) => {
   }
 }
 
-
-export default withAuthRedirect(connect(mapStateToProps, {
-  follow,unfollow, setCurrentPage, toggleFollowingProgress, getUsers})(UsersContainer))
+export default compose(withAuthRedirect,connect(mapStateToProps, {
+  follow,unfollow, setCurrentPage, toggleFollowingProgress, getUsers}))(UsersContainer)
