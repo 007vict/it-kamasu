2667abe7c0397ace1f0e9eb20a93712c7edd8ffa
007vict <super_vg@bk.ru>
2021-08-03 16:22:13 +0300

    stopSubmit(redux-form)79

--- src/components/Profile/ProfileContainer.js
+++ src/components/Profile/ProfileContainer.js
@@ -10,7 +10,7 @@ class ProfileContainer extends React.Component {
   componentDidMount() {
     let userID = this.props.match.params.userID
     if (!userID) {
-      userID = 2
+      userID = this.props.authorizedUserId
     }
     this.props.getUserProfile(userID)
     this.props.getStatus(userID)
@@ -27,7 +27,9 @@ class ProfileContainer extends React.Component {
 
 let mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
-  status: state.profilePage.status
+  status: state.profilePage.status,
+  authorizedUserId: state.auth.userID,
+  isAuth: state.auth.isAuth
 })
 
 
