453f1720192418f6af798ca224cc823ed530a474
007vict <super_vg@bk.ru>
2021-08-04 15:42:42 +0300

    reselect_part_2_3_82

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -1,6 +1,12 @@
-export const getUsers = (state) => {
+import {createSelector} from "reselect";
+
+const getUsersSelectors = (state) => {
   return state.usersPage.users
 }
+
+export const getUsers = createSelector(getUsersSelectors, users => {
+  return users.filter(u => true)
+})
 
 export const getPageSize = (state) => {
   return state.usersPage.pageSize
